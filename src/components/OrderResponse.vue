ab981b1fef9983bf240844608e7c0b2bc0379e66
Maxim Kuzin <iskait04@gmail.com>
2023-02-10 20:01:25 +0300

    Компонент для воспроизведения ответа от сервера

---
+++ src/components/OrderResponse.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  responseFromServer: string;
+}>();
+
+const emit = defineEmits<{
+  (event: "close-form"): void;
+}>();
+</script>
+
+<template>
+  <div
+    class="fixed inset-0 bg-gray/60 overflow-auto grid place-items-center"
+    @click.self="emit('close-form')"
+  >
+    <div class="bg-white p-10 rounded-lg relative">
+      <div v-html="responseFromServer"></div>
+      <span @click="emit('close-form')" class="absolute top-5 right-5">X</span>
+    </div>
+  </div>
+</template>
